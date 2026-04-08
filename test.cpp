246c61656242827e3e4f5bf37081307e81d28b8e
Szlesniak <szymles44@gmail.com>
2026-04-08 23:11:00 +0200

    blocklist checking

--- test.cpp
+++ test.cpp
@@ -1,11 +1,12 @@
-#include <iostream>
 #include <cstring>
-#include <sys/socket.h>
+#include <iostream>
 #include <netinet/in.h>
-#include <unistd.h>
 #include <string>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <unordered_set>
 
-std::string read_dns(const char* buffer, int bytes_received) {
+std::string read_dns(const char *buffer, int bytes_received) {
   if (bytes_received <= 12) {
     return "";
   }
@@ -24,17 +25,17 @@ std::string read_dns(const char* buffer, int bytes_received) {
       offset++;
     }
     domain += ".";
-
   }
-  if (!domain.empty()){
+  if (!domain.empty()) {
     domain.pop_back();
   }
   return domain;
 }
 
 int main() {
+  std::unordered_set<std::string> blacklist = {"ads.google.com", "wp.pl"};
   int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-  if (sockfd < 0){
+  if (sockfd < 0) {
     std::cerr << "Error while creating socket!" << std::endl;
     return 1;
   }
@@ -45,8 +46,8 @@ int main() {
   server_addr.sin_addr.s_addr = INADDR_ANY;
   server_addr.sin_port = htons(53);
 
-
-  if (bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+  if (bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) <
+      0) {
     std::cerr << "Error while binding port" << std::endl;
     return 1;
   }
@@ -54,13 +55,21 @@ int main() {
   struct sockaddr_in client_addr;
   socklen_t client_len = sizeof(client_addr);
 
-  while(true) {
-    int bytes_received = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)&client_addr, &client_len);
+  while (true) {
+    int bytes_received = recvfrom(sockfd, buffer, sizeof(buffer), 0,
+                                  (struct sockaddr *)&client_addr, &client_len);
     if (bytes_received > 0) {
       std::string query = read_dns(buffer, bytes_received);
 
-      if(!query.empty()) {
-        std::cout << "DNS Query for: " << query << std::endl;
+      if (!query.empty()) {
+        if (query.find("in-addr.arpa") == std::string::npos) {
+          if (blacklist.find(query) != blacklist.end()) {
+            std::cout << "Blocking ad domain:" << query << std::endl;
+
+          } else {
+            std::cout << "Passing clear domain: " << query << std::endl;
+          }
+        }
       }
     }
   }
