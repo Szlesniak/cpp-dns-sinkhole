e28e04b7dcfd0632e4fb0b408feadd620c2c8622
Szlesniak <szymles44@gmail.com>
2026-04-09 23:30:16 +0200

    separate testing and working file

--- test.cpp
+++ test.cpp
@@ -1,7 +1,5 @@
 #include <arpa/inet.h>
-#include <chrono>
 #include <cstring>
-#include <fstream>
 #include <iostream>
 #include <netinet/in.h>
 #include <string>
@@ -9,32 +7,6 @@
 #include <sys/time.h>
 #include <unistd.h>
 #include <unordered_set>
-
-void insert_list(const std::string &filename,
-                 std::unordered_set<std::string> &blacklist) {
-  std::ifstream file(filename);
-  if (!file.is_open()) {
-    std::cerr << "Warning: the file could not be opened " << filename
-              << std::endl;
-    return;
-  }
-  std::string line;
-  int count = 0;
-
-  while (std::getline(file, line)) {
-    if (!line.empty() && line.back() == '\r') {
-      line.pop_back();
-    }
-    if (line.empty() || line[0] == '#') {
-      continue;
-    }
-    blacklist.insert(line);
-    count++;
-  }
-  std::cout << "Loaded " << count
-            << " of blocked domains from file: " << filename << std::endl;
-  file.close();
-}
 
 std::string read_dns(const char *buffer, int bytes_received) {
   if (bytes_received <= 12) {
@@ -63,15 +35,7 @@ std::string read_dns(const char *buffer, int bytes_received) {
 }
 
 int main() {
-  std::unordered_set<std::string> blacklist;
-  std::cout << "Loading blocked domains..." << std::endl;
-  auto start = std::chrono::high_resolution_clock::now();
-  insert_list("blocklist.txt", blacklist);
-
-  auto stop = std::chrono::high_resolution_clock::now();
-  auto duration =
-      std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
-
+  std::unordered_set<std::string> blacklist = {"ads.google.com", "wp.pl"};
   int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
   if (sockfd < 0) {
     std::cerr << "Error while creating socket!" << std::endl;
@@ -102,6 +66,7 @@ int main() {
       if (!query.empty()) {
         if (query.find("in-addr.arpa") == std::string::npos) {
           if (blacklist.find(query) != blacklist.end()) {
+            std::cout << "Blocking ad domain:" << query << std::endl;
             char response[512];
             memcpy(response, buffer, bytes_received);
             int response_len = bytes_received;
@@ -140,8 +105,10 @@ int main() {
 
             sendto(sockfd, response, response_len, 0,
                    (struct sockaddr *)&client_addr, client_len);
+            std::cout << "Blocked" << std::endl;
 
           } else {
+            std::cout << "Passing clear domain: " << query << std::endl;
             int upstream_sock = socket(AF_INET, SOCK_DGRAM, 0);
             struct timeval tv;
             tv.tv_sec = 2;
@@ -165,6 +132,8 @@ int main() {
             if (upstream_bytes > 0) {
               sendto(sockfd, upstream_buffer, upstream_bytes, 0,
                      (struct sockaddr *)&client_addr, client_len);
+              std::cout << " Answered the original query to Windows ("
+                        << upstream_bytes << "bytes)" << std::endl;
 
             } else {
               std::cout << "Error: Timeout or lack of answer from 8.8.8.8"
